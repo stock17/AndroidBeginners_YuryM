240d202c6e8ee06276584e3949eed501bf6c8fdd
Yury <31337@mail.ru>
2017-04-19 17:36:55 +0400

    Add RecyclerView

--- app/src/main/java/com/example/yury/myflashcardsproject/MainActivity.java
+++ app/src/main/java/com/example/yury/myflashcardsproject/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.yury.myflashcardsproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        RecyclerView rvCardList = (RecyclerView) findViewById(R.id.rvCardList);
+        rvCardList.setHasFixedSize(true);
+//        RecyclerView.LayoutManager manager = new LinearLayoutManager(this);
+        LinearLayoutManager manager = new LinearLayoutManager(this);
+        rvCardList.setLayoutManager(manager);
+        RecyclerView.Adapter adapter = new RVAdapter();
+        rvCardList.setAdapter(adapter);
+
+
     }
 }
---
+++ app/src/main/java/com/example/yury/myflashcardsproject/RVAdapter.java
@@ -0,0 +1,52 @@
+package com.example.yury.myflashcardsproject;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+/**
+ * Created by Yury on 19.04.2017.
+ */
+
+public class RVAdapter extends RecyclerView.Adapter<RVAdapter.ViewHolder>{
+
+    public String[] textdata;
+
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public TextView tvListPointText;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            tvListPointText = (TextView) itemView.findViewById(R.id.tvListPointText);
+        }
+    }
+
+    public RVAdapter () {
+        textdata = new String[] {"menu 1", "menu 2", "menu 3"};
+        // TODO
+    }
+
+    @Override
+    public RVAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_point, parent, false);
+        ViewHolder holder = new ViewHolder(v);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(RVAdapter.ViewHolder holder, int position) {
+
+        holder.tvListPointText.setText(textdata[position]);
+    }
+
+    @Override
+    public int getItemCount() {
+        return textdata.length;
+    }
+
+
+}
